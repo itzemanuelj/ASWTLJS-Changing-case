c9a84d21c35d1011c146fed976566e039598eb73
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-24 12:48:26 -0500

    mvp commit for case changes

---
+++ app.js
@@ -0,0 +1,53 @@
+///Type the characters that are missing from this code.
+let allLower = userInput.toLowerCase();
+///()
+
+///Type the character that is missing from this code.
+let allLowerCase = userInput.toLowerCase();
+///.
+
+///two the character thatTwo characters in this code are incorrect. Type the two correct characters.
+let allLowercase = userInput.toLowerCase();
+///toLowerCase
+
+///What is the value of lowerVersion? Include the quotation marks in your answer.
+var lowerVersion = "NO!".toLowerCase();
+
+console.log(lowerVersion);
+///no!
+
+///convert the string represented by cityToCheck to lower-case and assign the converted string to the same variable.
+
+let cityToCheck = cityToCheck.toLowerCase();
+
+///Convert the string represented by upperString to lower-case and assign the result to lowerString, which hasn't been declared beforehand.
+
+let upperString = lowerString.toLowerCase();
+
+///Convert the string represented by x to lower-case and assign the result to the same variable. 
+
+let x = x.toLowerCase();
+
+///Convert the string represented by a variable to lower-case and assign the result to a second variable that hasn't been declared beforehand.
+
+let a = b.toLowerCase();
+
+///Convert the string represented by a variable to upper-case and assign the result to a second variable that hasn't been declared beforehand.          
+
+let c = d.toUpperCase();
+
+///Convert a string to lower-case and assign the result to a variable that has been declared beforehand.
+
+let lname = 'jones'.toLowerCase();
+
+///Display in an alert the upper-case version of the string represented by a variable.
+
+let fname = 'dan'.toUpperCase();
+alert(fname);
+///DAN
+
+alert(message.toUpperCase());
+
+///Convert the string represented by an array element to lower-case and assign it to a variable that hasn't been declared beforehand.
+
+let names = names[1].toLowerCase();
